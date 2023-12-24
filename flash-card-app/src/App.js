626174a35ff600dc34a9d10f475fec561f6b1420
Ali Amrahli <ali.emrahli@gmail.com>
2023-12-24 22:18:34 +0400

    Fixed error when starting local react app. Removed unnecessary files. Scratch files added.

---
+++ flash-card-app/src/App.js
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div className="App">
+      <a href="../public/index.html" target="_blank">Home Page</a>
+      <a href="" target="_blank">Flash Cards</a>
+    </div>
+  );
+}
+
+export default App;
